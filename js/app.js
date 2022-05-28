19f8e041ba5ce39b85a161104347c2e8a0c7887d
cs1369 <suarezgabriel420@gmail.com>
2022-05-28 20:52:25 -0300

    section uno

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
 let li = document.querySelectorAll(".nav__menu-items");
 let menu= document.querySelector(".nav__iconMenu");
+let nav__menu = document.querySelector(".nav__menuH0");
 
 menu.addEventListener("click",()=>{
+    if(nav__menu.classList.contains("nav__menuH0")){
+        nav__menu.classList.remove("nav__menuH0");
+        nav__menu.classList.add("nav__menuH100");
+
+    }
+    else{
+        nav__menu.classList.remove("nav__menuH100");
+        nav__menu.classList.add("nav__menuH0"); 
+    }
     for(let lista of li){
         lista.classList.toggle("animaciones-nav");
     }
