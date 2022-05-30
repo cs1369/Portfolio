0608206804d1843d596cccd6e535530a276306e9
cs1369 <suarezgabriel420@gmail.com>
2022-05-29 23:27:37 -0300

    principio de btn en js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,8 @@
 let li = document.querySelectorAll(".nav__menu-items");
 let menu= document.querySelector(".nav__iconMenu");
 let nav__menu = document.querySelector(".nav__menuH0");
+const btnskills = document.querySelector(".skills__sobreMi");
+
 
 menu.addEventListener("click",()=>{
     if(nav__menu.classList.contains("nav__menuH0")){
