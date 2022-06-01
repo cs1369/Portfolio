b6d876c8a0af75f52a4ebec347814b69f6f1842f
cs1369 <suarezgabriel420@gmail.com>
2022-06-01 02:41:27 -0300

    fin de section-sobreMi

--- js/app.js
+++ js/app.js
@@ -2,7 +2,8 @@ let li = document.querySelectorAll(".nav__menu-items");
 let menu= document.querySelector(".nav__iconMenu");
 let nav__menu = document.querySelector(".nav__menuH0");
 const btnskills = document.querySelector(".skills__sobreMi");
-
+const card1 = document.querySelector(".card360__sobreMi1"), card2=document.querySelector(".card360__sobreMi2");
+const html =document.querySelector(".html"),css=document.querySelector(".css"),js=document.querySelector(".js"),bts=document.querySelector(".bts");
 
 menu.addEventListener("click",()=>{
     if(nav__menu.classList.contains("nav__menuH0")){
@@ -16,5 +17,37 @@ menu.addEventListener("click",()=>{
     }
     for(let lista of li){
         lista.classList.toggle("animaciones-nav");
+    }
+});
+btnskills.addEventListener("click",()=>{
+    if(card1.classList.contains("animation-card1") || card1.classList.contains("animation-card2")){
+        if(card1.classList.contains("animation-card1")){
+            card1.classList.remove("animation-card1");
+            card1.classList.add("animation-card2");
+            card2.classList.remove("animation-card2");
+            card2.classList.add("animation-card1");
+        }else{
+            card2.classList.remove("animation-card1");
+            card2.classList.add("animation-card2");
+            card1.classList.remove("animation-card2");
+            card1.classList.add("animation-card1");
+        }
+    }
+    else{
+        card1.classList.add("animation-card1");
+        card2.classList.add("animation-card2");
+    }
+    if(btnskills.textContent == "Skills"){
+        btnskills.textContent ="Sobre Mi";
+        html.classList.add("animation-html");
+        css.classList.add("animation-css");
+        js.classList.add("animation-js");
+        bts.classList.add("animation-bts");
+    }else{
+        btnskills.textContent = "Skills";
+        html.classList.remove("animation-html");
+        css.classList.remove("animation-css");
+        js.classList.remove("animation-js");
+        bts.classList.remove("animation-bts");
     }
 });
